bb3aba27d230b305d06c97846a7baa035448cbe0
awkrail <taichitary@gmail.com>
2025-09-26 18:41:04 +0900

    implemented padding

--- src/fp32/mnist_conv.cpp
+++ src/fp32/mnist_conv.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <vector>
+#include <string.h>
 
 #include "mnist_conv.h"
 #include "data_7.h"
@@ -11,6 +12,8 @@ public:
     MnistConv(const std::vector<float> & conv1_weight, const std::vector<float> & conv1_bias,
               const std::vector<float> & fc1_weight, const std::vector<float> & fc1_bias,
               const std::vector<float> & fc2_weight, const std::vector<float> & fc2_bias);
+
+    std::vector<float> padding(std::vector<float> & data);
     int forward(std::vector<float> & data);
 
 public:
@@ -28,36 +31,32 @@ public:
     const int output_channel_num = 5;
     const int kernel_size = 3;
     const int stride = 1;
-    const int paddding = 1;
-    constexpr int fc1_hidden_size = 32 * 28 * 28;
+    const int pad_size = 1;
+    const int fc1_hidden_size = 32 * 28 * 28;
     const int fc2_hidden_size = 1024;
 };
 
 MnistConv::MnistConv(const std::vector<float> & conv1_weight, const std::vector<float> & conv1_bias,
                      const std::vector<float> & fc1_weight, const std::vector<float> & fc1_bias,
-                     const std::vector<float> & fc2_weight, const std::vector<float> & fc2_bias) :
+                     const std::vector<float> & fc2_weight, const std::vector<float> & fc2_bias)
     : conv1_weight{conv1_weight}, conv1_bias{conv1_bias}, fc1_weight{fc1_weight}, fc1_bias{fc1_bias},
       fc2_weight{fc2_weight}, fc2_bias{fc2_bias} {}
 
-MnistConv::padding(std::vector<float> & data)
+std::vector<float> MnistConv::padding(std::vector<float> & data)
 {
     std::vector<float> padded_data(padded_image_size * padded_image_size, 0.0f);
     for (int i = 0; i < image_size; i++)
     {
-        float * dst = &padded_data[(i+1) * padded_image_size + 1];
+        float * dst = &padded_data[(i + pad_size) * padded_image_size + pad_size];
         float * src = &data[i * image_size];
-        std::memcpy(dst, src, image_size * sizeof(float));
+        memcpy(dst, src, image_size * sizeof(float));
     }
     return padded_data;
 }
 
-MnistConv::forward(std::vector<float> & data)
+int MnistConv::forward(std::vector<float> & data)
 {
     data = padding(data);
-    for (auto & d : data)
-    {
-        std::cout << d << " ";
-    }
     return 0;
 }
 

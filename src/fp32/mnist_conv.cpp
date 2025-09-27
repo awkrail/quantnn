81ae6d093b4a6c76b48a84c35f79cd14fae4fe34
awkrail <taichitary@gmail.com>
2025-09-27 17:54:12 +0900

    implemented conv layer

--- src/fp32/mnist_conv.cpp
+++ src/fp32/mnist_conv.cpp
@@ -14,6 +14,7 @@ public:
               const std::vector<float> & fc2_weight, const std::vector<float> & fc2_bias);
 
     std::vector<float> padding(std::vector<float> & data);
+    std::vector<float> conv1(std::vector<float> & data);
     int forward(std::vector<float> & data);
 
 public:
@@ -32,7 +33,7 @@ public:
     const int kernel_size = 3;
     const int stride = 1;
     const int pad_size = 1;
-    const int fc1_hidden_size = 32 * 28 * 28;
+    const int fc1_hidden_size = 5 * 28 * 28;
     const int fc2_hidden_size = 1024;
 };
 
@@ -54,9 +55,45 @@ std::vector<float> MnistConv::padding(std::vector<float> & data)
     return padded_data;
 }
 
+std::vector<float> MnistConv::conv1(std::vector<float> & data)
+{
+    const int output_size = image_size - kernel_size + 2 * pad_size + 1;
+    std::vector<float> output (output_channel_num * output_size * output_size);
+    const int oW_size = padded_image_size - kernel_size + 1;
+    const int oH_size = padded_image_size - kernel_size + 1;
+    for (int o = 0; o < output_channel_num; o++)
+    {
+        for (int i = 0; i < oH_size; i++)
+        {
+            for (int j = 0; j < oW_size; j++)
+            {
+                float val = 0.0f;
+                for (int k = 0; k < kernel_size; k++)
+                {
+                    for (int l = 0; l < kernel_size; l++)
+                    {
+                        int target_index = (i + k) * padded_image_size + (j + l);
+                        int weight_index = o * kernel_size * kernel_size + kernel_size * k + l;
+                        val += data[target_index] * conv1_weight[weight_index];
+                    }
+                }
+                int output_index = o * oH_size * oW_size + i * oW_size + j;
+                output[output_index] = val + conv1_bias[o];
+            }
+        }
+    }
+    return output;
+}
+
 int MnistConv::forward(std::vector<float> & data)
 {
     data = padding(data);
+    data = conv1(data);
+    for (auto & d : data)
+    {
+        std::cout << d << " ";
+    }
+    std::cout << std::endl;
     return 0;
 }
 
